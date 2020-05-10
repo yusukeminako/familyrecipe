28dcc8e6ea4b47143026684b5e052710633f5996
yusukeminako <yusuke19881016aspect@yahoo.co.jp>
2020-05-10 14:13:46 +0900

    a

--- app/assets/javascripts/servise.js
+++ app/assets/javascripts/servise.js
@@ -1,9 +1,13 @@
-$(function(){
-  var position = $('.service').offset().top;
-  $('.banner').click(function () {
-    $('body,html').animate({
-      scrollTop: position
-    });
-    return false;
-  });
+$('a[href^="#"]').click(function() {
+  // スクロールの速度
+  var speed = 400; // ミリ秒で記述
+  var href = $(this).attr("href");
+  // atterメソッドは属性を取得したり、設定したりする
+  // classやhrefは属性
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $('body,html').animate({
+    scrollTop: position
+  }, speed, 'swing');
+  return false;
 });
